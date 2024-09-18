9727294251c607eba42b3a66ff8ce64994df64cd
joegeorge022 <127773439+joegeorge022@users.noreply.github.com>
2024-09-18 23:19:47 +0530

    Add files via upload

---
+++ Python Project/main(3).py
@@ -0,0 +1,9 @@
+# 5.USER INPUT
+name = input("Enter your name: ")
+
+print(name)
+
+
+# Input is used when user input is required in a program. In this user input for name is asked first and then it is printed.
+
+print(f"Hello{name}!")
