21f23bd9a3a7f3eb45754a1b89776f83ce4a263e
joegeorge022 <127773439+joegeorge022@users.noreply.github.com>
2024-09-19 19:00:26 +0530

    Add files via upload

---
+++ main(1).py
@@ -0,0 +1,70 @@
+# This is my first Python code.
+# Start Date: 18 September 2024
+#Contains: (Print, Variables, Arithmetic)
+
+# 1.PRINT
+print("I love pizza")
+print("It's really good")
+
+
+
+# 2.VARIABLES
+full_name = "Joe George"
+age = "18"
+is_student = True
+
+print(f"Hello {full_name}!")
+print(f"Your name is {full_name} and you are {age} years old.")
+print(f"Are you a student? Ans: {is_student}")
+
+if is_student:
+    print("He is a student")
+else:
+    print("He is not a student")
+
+
+
+# 3.ARITHMETIC
+#Addition
+friends = 5              #now printing friends =5
+
+friends +=5              #now printing friends= 5+5 =10
+                         #another way is (friends = friends + 5)
+print(friends)
+
+#Subtraction
+enemies = 10
+
+enemies -= 5      #now printing enemies= 10-5 =5
+                         #another way is (enemies = enemies - 5)
+print(enemies)
+
+#Multiplication
+allies = 10
+
+allies *= 2              #now printing allies= 5*2 =10
+                         #another way is (allies = allies*2)
+print(allies)
+
+#Division
+foes = 10
+
+foes /= 2                #now printing foes= 10/2 =5
+                         #another way is (foes = foes / 2)
+print(foes)
+
+#Integer Division
+# (Same shit as division but it rounds off to the nearest integer)
+rivals = 10
+
+rivals //= 3             #now printing rivals =10/3 ≈3
+                         #another way is (rivals = rivals//3)
+print(rivals)
+
+#Remainder
+guys = 10
+
+guys %= 3                #now printing guys = 10/3 remainder=1
+                         #another way is (guys = guys%3)
+print(guys)
+
